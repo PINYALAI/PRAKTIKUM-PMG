b4aaf1251389348595bb683f9250ae1207d7f87f
PINYALAI <alfurqonikazd887@gmail.com>
2026-03-31 21:33:24 +0700

    perulangan for

--- looping.cpp
+++ looping.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 #include <ctime>
 using namespace std;
+
+int main(){
+    int x;
+
+    //perulangan dengan for
+    //mencetak tulisan sebanyak 5 kali
+    cout<<"Perulangan for"<<endl;
+    for(int i=0; i<4; i++){
+        cout<<"TEKNOLOGI INFORMASI UMY"<<endl;    
+}
+cout<<endl;
+//untuk mendapatkan urutan acak yang berbeda
+srand(time(0));
